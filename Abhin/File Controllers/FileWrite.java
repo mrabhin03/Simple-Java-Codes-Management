19153ae059d4b4379bbe81ad5e5722004aa9a097
System 05 <system05pentium@gmail.com>
2024-11-29 11:14:09 +0530

    File controller added

--- Abhin/File Controllers/FileWrite.java
+++ Abhin/File Controllers/FileWrite.java
@@ -2,13 +2,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
 
-class FileCreate{
+class FileWrite{
     public static void main(String ar[]){
         try{
             FileWriter writer=new FileWriter("Files/Test1");
             writer.write("HELLO");
-            
-            writer.write("HELLO1");
             writer.close();
         }catch(IOException io){
             System.out.println("ERROR");
