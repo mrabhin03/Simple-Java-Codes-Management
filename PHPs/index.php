4a63288fbeb3089719360308373e24feb20c6c76
Abhin M <abhinm24@gmail.com>
2024-11-30 17:30:36 +0530

    Small Change

--- PHPs/index.php
+++ PHPs/index.php
@@ -10,7 +10,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <link href="css/styles.css?v=<?php echo time()?>" rel="stylesheet" />
     </head>
-    <body>=
+    <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#!">Java Code Hub</a>
