a3292eaf8a372e2a895bbb626361f948c4cddeb4
Abhin M <abhinm24@gmail.com>
2024-11-30 17:29:28 +0530

    Small Change

--- PHPs/index.php
+++ PHPs/index.php
@@ -64,7 +64,7 @@
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Java Code Hub</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Java Code Repository</p>
+                    <p class="lead fw-normal text-white-50 mb-0">A Java Code Repository</p>
                     <input type="text" id='SearchDataInput' class="SearchData" placeholder='Search Java File' onkeydown='ReadSerch(this)'>
                 </div>
                 
