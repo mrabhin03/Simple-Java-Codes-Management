88b28ffb256193c1178dc8bd60afe889531d10ed
System 05 <system05pentium@gmail.com>
2024-12-03 10:36:20 +0530

    File Read Added

---
+++ Abhin/File Controllers/FileRead.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+class FileRead{
+    public static void main(String ar[]){
+        try{
+            File f1=new File("Files/Test1");
+            Scanner read=new Scanner(f1);
+            while(read.hasNextLine()){
+                System.out.println(read.nextLine());
+            }
+        }catch(Exception e){
+            
+        }
+    }
+}
