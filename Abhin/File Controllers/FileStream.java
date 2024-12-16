afaf514cf5cc7359e4c9d26c73df7276a8d87d62
System 05 <system05pentium@gmail.com>
2024-12-16 11:08:14 +0530

    FileStream Added

--- Abhin/File Controllers/FileStream.java
+++ Abhin/File Controllers/FileStream.java
@@ -2,7 +2,7 @@ import java.io.FileOutputStream;
 public class FileStream {
     public static void main(String ar[]){
             try{
-                FileOutputStream file=new FileOutputStream("Files/Test2");
+                FileOutputStream file=new FileOutputStream("Files/Test2.txt");
                 file.write(65);
                 file.close();
         }catch(Exception e){
